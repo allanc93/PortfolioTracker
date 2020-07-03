3dca99bd4bc1cee5f14c54e0580fc71fb62baf73
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-07-03 15:21:47 +0100

    Changed Search.js to Input.js

--- src/components/ShareDisplay.js
+++ src/components/ShareDisplay.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ShareInfo from './ShareInfo';
 
 class ShareDisplay extends React.Component {
+
     render() {
         return (
             <div className="share-table">
