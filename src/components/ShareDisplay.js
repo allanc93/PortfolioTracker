8188e1e52b65b94be949289ac41fe1833b91d933
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-07-06 14:30:47 +0100

    Updates to ShareInfo.js and ShareDisplay.js to display API data

--- src/components/ShareDisplay.js
+++ src/components/ShareDisplay.js
@@ -2,25 +2,37 @@ import React from 'react';
 import ShareInfo from './ShareInfo';
 
 class ShareDisplay extends React.Component {
-
     render() {
         return (
             <div className="share-table">
                 <table>
-                    <tr>
-                        <th>Company Name</th>
-                        <th>Symbol</th>
-                        <th>High</th>
-                        <th>Low</th>
-                        <th>Price</th>
-                        <th>XYZ</th>
-                    </tr>
-
+                    <thead>
+                        <tr>
+                            <th>Symbol</th>
+                            <th>Company Name</th>
+                            <th>Type</th>
+                            <th>Region</th>
+                            <th>Currency</th>
+                        </tr>
+                    </thead>
                     {/* Loop through share results */}
-                    <ShareInfo />
+                    <ShareInfo shares={this.state.shares} />
                 </table>
             </div>
         );
+    }
+
+    state = {
+        shares: []
+    }
+
+    componentDidMount() {
+        fetch('https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=AAPL&interval=1min&apikey=BC34PVP226M1KDMR&outputsize=compact')
+            .then(response => response.json())
+            .then((data) => {
+                this.setState({ shares: data.bestMatches })
+            })
+            .catch(console.log)
     }
 }
 
--- src/components/ShareInfo.js
+++ src/components/ShareInfo.js
@@ -1,20 +1,43 @@
 import React from 'react';
 
-class ShareInfo extends React.Component {
-    render() {
-        // const company = this.props;
-        return (
-            <tr>
-                {/* Result for each share displayed in individual row */}
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>3</td>
-                <td>3</td>
-                <td>3</td>
-            </tr>
-        );
-    }
-}
+// class ShareInfo extends React.Component {
+//     render() {
+//         // const company = this.props;
+//         return (
+//             <tr>
+//                 {/* Result for each share displayed in individual row */}
+//                 <td>1</td>
+//                 <td>2</td>
+//                 <td>3</td>
+//                 <td>3</td>
+//                 <td>3</td>
+//                 <td>3</td>
+//             </tr>
+//         );
+//     }
+// }
+
+const ShareInfo = ({ shares }) => {
+    shares = Object.entries(shares)
+    console.log(typeof shares)
+    console.log(shares[0])
+
+    return (
+        <tbody>
+
+            {shares.map((share) => (
+                <tr>
+                    <td>{share[1]["1. symbol"]}</td>
+                    <td>{share[1]["2. name"]}</td>
+                    <td>{share[1]["3. type"]}</td>
+                    <td>{share[1]["4. region"]}</td>
+                    <td>{share[1]["8. currency"]}</td>
+                </tr>
+
+            ))
+            }
+        </tbody >
+    )
+};
 
 export default ShareInfo;
