e61cc158af236f8067057c64c1f8cb6e41edd7ab
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-07-08 15:49:05 +0100

    Update ShareDisplay.js
    Added comments and a temporary loading icon function

--- src/components/ShareDisplay.js
+++ src/components/ShareDisplay.js
@@ -10,6 +10,7 @@ class ShareDisplay extends React.Component {
     // }
     constructor() {
         super();
+        // Declare ShareDisplay component states
         this.state = {
             sharesData: [],
             shareNumber: 0
@@ -24,17 +25,24 @@ class ShareDisplay extends React.Component {
         //     })
         //     .catch(console.log)
         this.getDataFromAPI();
+
     }
 
     async getDataFromAPI() {
+        // Retrieve API data
         const APIlink = `testSharesData.json`;
         // const APIlink = `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=AAPL&interval=1min&apikey=BC34PVP226M1KDMR&outputsize=compact`;
         const resp = await axios.get(APIlink);
         let responseData = Object.entries(resp.data);
+
+        // Logs displayed before loop has started
         console.log("BEFORE LOOP");
         console.log(responseData);
+
+        // Loop through the data in the API response array
         responseData[0][1].forEach((result) => {
             this.setState({
+                // Define the name/value pairs to add to the sharesData state
                 sharesData: this.state.sharesData.concat({
                     name: result['2. name'],
                     symbol: result['1. symbol'],
@@ -44,14 +52,31 @@ class ShareDisplay extends React.Component {
                     timezone: result['7. timezone'],
                     open: result['5. marketOpen'],
                     close: result['6. marketClose'],
-                })
+                }),
+                // Add 1 to shareNumber state
+                shareNumber: this.state.shareNumber + 1
             });
+            // Logs displayed during loop
             console.log("IN LOOP");
             console.log(result);
             console.log(this.state.sharesData);
+
+            // Call checkShareNumber, to check if loading icon should still be displayed
+            this.checkShareNumber();
         })
+        // Logs displayed after loop has completed
         console.log("AFTER LOOP");
         console.log(this.state.sharesData);
+    }
+
+    // Check the value of the shareNumber state to determine if all results have been returned
+    checkShareNumber() {
+        if (this.state.shareNumber === 7) {
+            // Included setInterval to ensure icon is displayed for 3s after loading is complete
+            setInterval(() => {
+                document.getElementById('loading').style.display = 'none';
+            }, 3000);
+        }
     }
 
     render() {
@@ -73,11 +98,11 @@ class ShareDisplay extends React.Component {
                     {/* Return share results */}
                     {/* <ShareInfo shares={this.state.sharesData} /> */}
                 </table>
-                {/* <div id="loading" className="text-center">
-                    <div class="spinner-border text-secondary " role="status">
-                        <span class="sr-only">Loading...</span>
+                <div id="loading" className="text-center">
+                    <div className="spinner-border text-secondary " role="status">
+                        <span className="sr-only">Loading...</span>
                     </div>
-                </div> */}
+                </div>
             </div>
         );
     }
