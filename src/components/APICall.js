d7d9e235f595e70bf815b850ca34c0b1196b7ab7
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-07-09 18:27:43 +0100

    Update APICall.js
    Tidied up code.

--- src/components/APICall.js
+++ src/components/APICall.js
@@ -7,7 +7,6 @@ let classData = {
 
 async function APICall(APIfunction, APIsearch) {
 
-
     let param = '';
     APIfunction === "SYMBOL_SEARCH" ? param = 'keywords' : param = 'symbol';
     const APIlink = `https://www.alphavantage.co/query?function=${APIfunction}&${param}=${APIsearch}&interval=1min&apikey=BC34PVP226M1KDMR&outputsize=compact`;
@@ -23,16 +22,14 @@ async function APICall(APIfunction, APIsearch) {
             callMap: classData.callMap.concat([{ APIlink, resp }])
         };
 
-        setTimeout(removeOldData.bind(null,APIlink), 60000);
-
+        setTimeout(removeOldData.bind(null, APIlink), 60000);
 
         return resp;
     }
-
 }
 
 function findValue(arr, key) {
-    for(let i = 0; i <arr.length; i++){
+    for (let i = 0; i < arr.length; i++) {
         if (arr[i]) {
             let bool = (arr[i].APIlink === key);
             if (bool) {
@@ -42,26 +39,24 @@ function findValue(arr, key) {
     }
 }
 
-function removeOldData(APIlink){
+function removeOldData(APIlink) {
     console.log('removing old data');
 
-            // Remove the APIlink from usedCalls
-            for (let i = 0; i < classData.usedCalls.length; i++) {
-                console.log('looking at ' + classData.usedCalls[i]);
-                console.log('looking at ' + APIlink);
-                if (classData.usedCalls[i] === APIlink) {
-                    
-                    classData.usedCalls.splice(i, 1);
-                }
-            }
+    // Remove the APIlink from usedCalls
+    for (let i = 0; i < classData.usedCalls.length; i++) {
+        console.log('looking at ' + classData.usedCalls[i]);
+        console.log('looking at ' + APIlink);
+        if (classData.usedCalls[i] === APIlink) {
+            classData.usedCalls.splice(i, 1);
+        }
+    }
 
-            //Remove the APIlink from callMap
-            for (let i = 0; i < classData.callMap.length; i++) {
-                if (classData.callMap[i].APIlink === APIlink) {
-                    classData.callMap.splice(i, 1);
-                }
-            }
-
+    //Remove the APIlink from callMap
+    for (let i = 0; i < classData.callMap.length; i++) {
+        if (classData.callMap[i].APIlink === APIlink) {
+            classData.callMap.splice(i, 1);
+        }
+    }
 }
 
 export default APICall;
