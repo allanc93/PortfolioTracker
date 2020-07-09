d308a671e6054897299090685abdc03041e053ee
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-07-09 12:10:02 +0100

    Update Input.js
    Working on search field

--- src/components/Input.js
+++ src/components/Input.js
@@ -3,29 +3,34 @@ import React, { Component } from 'react'
 class Input extends Component {
     constructor(props) {
         super(props);
-
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
-            searchQuery: ''
+            inputField: ''
         };
     }
 
-    handleSearchQuery = (e) => {
+    handleChange = (event) => {
         this.setState({
-            searchQuery: e.target.value
+            inputField: event.target.value
         });
     }
 
-    handleSubmit = (e) => {
-        alert(`${this.state.searchQuery}`);
-        e.preventDefault();
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.inputData(this.state.inputField);
+        this.setState({
+            inputField: ''
+        });
     }
 
     render() {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>Search by company name or symbol: </label>
-                <input type="text" value={this.state.searchQuery} onChange={this.handleSearchQuery} />
+                <input type="text" value={this.state.inputField} onChange={this.handleChange} />
                 <button type="submit">Submit</button>
+                <p>Received by CHILD: {this.state.inputField}</p>
             </form>
         )
     }
