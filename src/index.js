48e56ca784eb48e5c4425610c441e3343f6d07a5
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-07-10 16:57:04 +0100

    Update index.js
    Import jQuery

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap.min.css';
 //import './bootstrap.min.js';
-//import $ from jQuery;
+import 'jquery';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
