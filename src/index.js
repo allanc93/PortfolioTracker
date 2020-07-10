c2de3851d88d5e0bce6746839f1900bad51be259
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-07-10 14:12:54 +0100

    Update index.js
    Add Bootstrap.js import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap.min.css';
+import './bootstrap.min.js';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
