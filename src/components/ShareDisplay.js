44aa741de86bb76c3234de0beff117194f8ad2e9
allanc93 <45591897+allanc93@users.noreply.github.com>
2020-07-14 16:45:16 +0100

    Update ShareDisplay.js
    Removed uneccessary imports. Removed code for loading icon.

--- src/components/ShareDisplay.js
+++ src/components/ShareDisplay.js
@@ -1,12 +1,10 @@
 import React from 'react';
-// import axios from 'axios';
 
-import Input from './Input';
+import SearchInput from './SearchInput';
 import TableComponent from './TableComponent';
 import APICall from './APICall';
-import Button from './Button';
-import Modal from './Modal';
-//import '../modalJQ';
+import ButtonComponent from './ButtonComponent';
+import ViewModal from './ViewModal';
 
 class ShareDisplay extends React.Component {
     constructor(props) {
@@ -16,15 +14,16 @@ class ShareDisplay extends React.Component {
         // Declare ShareDisplay component states
         this.state = {
             sharesData: [],
-            shareNumber: 0,
+            resultData: [],
             searchData: '',
             modalTitle: '',
-            modalToken: ''
-
+            modalToken: '',
+            modalCurrency: '',
+            modalShow: false
         };
     }
 
-    handleSearchData(data) {
+    handleSearchData = (data) => {
         this.setState({
             sharesData: [],
             searchData: data
@@ -40,14 +39,35 @@ class ShareDisplay extends React.Component {
         }
     }
 
-    updateModal(title, token) {
-        console.log('updating modal to ' + title + ', ' + token);
+    async updateModal(title, token, currency) {
+        console.log(`Updating modal to ${title}/${token} (currency: ${currency}).`);
         this.setState({
             modalTitle: title,
-            modalToken: token
+            modalToken: token,
+            modalCurrency: currency,
+            modalShow: true
+        });
+
+        // Retrieve API data
+        const resp = await APICall('GLOBAL_QUOTE', token);
+        let responseData = Object.entries(resp.data);
+
+        this.setState({
+            // Define the name/value pairs to add to the responseData state
+            resultData: ({
+                symbol: responseData[0][1]['01. symbol'],
+                open: Number(responseData[0][1]['02. open']).toFixed(2),
+                high: Number(responseData[0][1]['03. high']).toFixed(2),
+                low: Number(responseData[0][1]['04. low']).toFixed(2),
+                price: Number(responseData[0][1]['05. price']).toFixed(2),
+                volume: responseData[0][1]['06. volume'],
+                latestTradingDay: responseData[0][1]['07. latest trading day'],
+                previousClose: Number(responseData[0][1]['08. previous close']).toFixed(2),
+                change: Number(responseData[0][1]['09. change']).toFixed(2),
+                changePercent: responseData[0][1]['10. change percent']
+            }),
         });
     }
-
 
     async getDataFromAPI(keyword) {
         // Retrieve API data
@@ -67,62 +87,24 @@ class ShareDisplay extends React.Component {
                     timezone: result['7. timezone'],
                     open: result['5. marketOpen'],
                     close: result['6. marketClose'],
-                    buy: <Button handleClick={() => { this.updateModal(result['2. name'], result['1. symbol']) }} buttonText='Buy' modal="true" token={`#${result['1. symbol']}`} />,
+                    Details: <ButtonComponent handleClick={() => { this.updateModal(result['2. name'], result['1. symbol'], result['8. currency']) }} buttonText='View' btnType="modalOpen" token={`#${result['1. symbol']}`} />,
                 }),
-                // Add 1 to shareNumber state
-                // shareNumber: this.state.shareNumber + 1
             });
-            // Call checkShareNumber, to check if loading icon should still be displayed
-            // this.checkShareNumber();
         })
     }
-
-    // Check the value of the shareNumber state to determine if all results have been returned
-    // checkShareNumber() {
-    //     if (this.state.shareNumber === 7) {
-    //         // Included setInterval to ensure icon is displayed for 3s after loading is complete
-    //         setInterval(() => {
-    //             if (document.getElementById('loading')) {
-    //                 document.getElementById('loading').style.display = 'none';
-    //             }
-    //         }, 3000);
-    //     }
-    // }
 
     render() {
         return (
             <div className="share-display">
                 <h1 className="my-4" >Shares Available</h1>
-                <Input inputData={this.handleSearchData} />
+                <SearchInput inputData={this.handleSearchData} />
                 <hr className="my-4" />
-
-
 
                 {(this.state.searchData === '') || (this.state.sharesData.length === 0)
                     ? <p>Enter a company name (eg, Apple) or it's symbol (eg, AAPL) to find results...</p>
                     : <TableComponent tableData={this.state.sharesData} />}
 
-
-
-                <Modal title={this.state.modalTitle} id={this.state.modalToken} />
-
-
-
-
-                {/* {((this.state.searchData === '') && (this.state.sharesData.length === 0))
-                    ? <p>Enter a company name (eg, Apple) or it's symbol (eg, AAPL) to find results...</p>
-                    : (this.state.sharesData.length > 0)
-                        ? <TableComponent tableData={this.state.sharesData} />
-                        : <p>No results were found, please try again.</p>} */}
-
-
-
-                {/* <TableComponent tableData={this.state.sharesData} /> */}
-                {/* <div id="loading" className="text-center">
-                    <div className="spinner-border text-secondary " role="status">
-                        <span className="sr-only">Loading...</span>
-                    </div>
-                </div> */}
+                <ViewModal title={this.state.modalTitle} id={this.state.modalToken} currency={this.state.modalCurrency} show={this.state.modalShow} onHide={() => this.setState({ modalShow: false })} modalData={this.state.resultData} />
             </div>
         );
     }
